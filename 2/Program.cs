75f0a7c1d2bc42c1baccb4123e3bf5fed9f18a9d
Egor <KrylovWork@yandex.ru>
2022-08-21 12:49:52 +0500

    initial commit

---
+++ 2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int[] array = new int[10];
+int oddIndexSum = 0;
+
+for (int i = 0; i < array.Length; i ++)
+{
+    array[i] = new Random().Next(11);
+    if (i % 2 == 1) oddIndexSum += array[i];
+}
+Console.WriteLine($"[{String.Join(", ", array)}]");
+Console.WriteLine($"Сумма элементов на нечентных позициях равна {oddIndexSum}");
---
+++ 3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+double[] array = new double[10];
+
+for (int i = 0; i < array.Length; i++) array[i] = Math.Round(new Random().NextDouble() * 100,3);
+
+double max = array[0];
+double min = array[0];
+
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max) max = array[i];
+    if (array[i] < min) min = array[i];
+}
+
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива равна {(max - min)}");
